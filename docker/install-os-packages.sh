4fa580194389083d6c7d06e6bb8cde56c438834b
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2026-02-07 10:27:11 +0800

    install emacs-lucid fonts-noto-cjk xpra

--- docker/install-os-packages.sh
+++ docker/install-os-packages.sh
@@ -4,6 +4,6 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install -y libicu-dev emacs-gtk
+apt-get install -y libicu-dev emacs-lucid fonts-noto-cjk xpra
 apt-get clean
 rm -rf /var/lib/apt/lists/*
